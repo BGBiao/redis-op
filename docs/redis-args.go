540cb9c05d8423c5e3f90575fbe61b7da0e32520
xuxuebiao <xuxuebiao@jd.com>
2018-12-31 16:36:44 +0800

    update the sort command

--- docs/redis-args.go
+++ docs/redis-args.go
@@ -1,30 +1,39 @@
 /**
  * @File Name: redis-args.go
- * @Author: xxbandy @http://xxbandy.github.io 
+ * @Author: xxbandy @http://xxbandy.github.io
  * @Email:
  * @Create Date: 2018-04-13 17:04:45
- * @Last Modified: 2018-05-06 15:05:29
+ * @Last Modified: 2018-12-31 16:12:02
  * @Description:
  */
 
 package main
+
 import (
-    "fmt"
-    "github.com/gomodule/redigo/redis"
-    redisop "github.com/xxbandy/redis-op/pool"
+	"fmt"
+	"github.com/gomodule/redigo/redis"
+	redisop "github.com/xxbandy/redis-op/pool"
 )
 
 func main() {
-    allip := "10.0.0.1"
-    pool := redisop.NewPool("localhost","32771","")
-    defer pool.Close()
-    conn := pool.Get()
-    defer conn.Close()
+	pool := redisop.NewPool("localhost", "32772", "")
+	defer pool.Close()
+	conn := pool.Get()
+	defer conn.Close()
 
-		lpushO,setErr := redis.Int(conn.Do("lpush",redis.Args{}.Add("ansible-key").AddFlat(allip)...))
-		if setErr == nil {
-				fmt.Printf("lpush %d ip:%s",lpushO,allip)
-		}
-
-
+	// sort uid desc by user_day_* get user_name_* get user_day_*
+	// lpushO, setErr := redis.Int(conn.Do("lpush", redis.Args{}.Add("ansible-key").AddFlat(allip)...))
+	// args := "desc by user_day_* get user_name_* get user_day_*"
+	/*
+	  args := map[string]string{
+	      "by":    					"user_day_*",
+	      "get":            "get user_name_*",
+	  }
+	*/
+	//sortv,sortErr := redis.Strings(conn.Do("sort", redis.Args{}.Add("uid desc").AddFlat(args)...))
+	sortv, sortErr := redis.Strings(conn.Do("sort", "uid", "desc", "by", "user_day_*", "get","#","get", "user_name_*", "get", "user_day_*"))
+	if sortErr != nil {
+		fmt.Printf("%s\n", sortErr)
+	}
+	fmt.Println(sortv)
 }
